d5afb90d2866aa7af9dcb025292bdf8e2fe7319f
Flávio Pereira <github@flaviopereira.dev>
2025-03-07 23:30:41 +0000

    sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
 					label: 'English',
 					lang: 'en',
 				},
-				'pt-pt': {
+				'pt': {
 					label: 'Português',
 					lang: 'pt-PT',
 				},
@@ -48,12 +48,93 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Honda',
+					label: 'Honda ECUs',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Honduino OBD2 DPFI - V5', slug: 'honda/honduino-obd2-dpfi' },
+						{ label: 'Honduino K', items: [
+							{ label: 'V1', link: '/honda/k/v1' },
+							{ label: 'V1.1', link: '/honda/k/v1.1' },
+						]},
+						{ label: 'Honduino OBD0', items: [
+							{ label: 'V1', link: '/honda/obd0/v1' },
+							{ label: 'V1.1', link: '/honda/obd0/v1.1' },
+						]},
+						{ label: 'Honduino OBD1', items: [
+							{ label: 'V2', link: '/honda/obd1/v2' },
+							{ label: 'V3', link: '/honda/obd1/v3' },
+							{ label: 'V3.1', link: '/honda/obd1/v3.1' },
+							{ label: 'V4', link: '/honda/obd1/v4' },
+							{ label: 'V4.1', link: '/honda/obd1/v4.1' },
+							{ label: 'V4.2', link: '/honda/obd1/v4.2' },
+							{ label: 'V4.3', link: '/honda/obd1/v4.3' },
+						]},
+						{ label: 'Honduino OBD1 DPFI', items: [
+							{ label: 'V1', link: '/honda/obd1-dpfi/v1' },
+						]},
+						{ label: 'Honduino OBD2 DPFI', items: [
+							{ label: 'V4', link: '/honda/obd2-dpfi/v4' },
+							{ label: 'V5', link: '/honda/obd2-dpfi/v5' },
+						]},
 					],
-				}
+				},
+				{
+					label: 'Nissan ECUs',
+					items: [
+						{ label: 'BossECU S13X', items: [
+							{ label: 'V2', link: '/nissan/s13x/v2' },
+							{ label: 'V3', link: '/nissan/s13x/v3' },
+						]},
+						{ label: 'BossECU S15X', items: [
+							{ label: 'V1', link: '/nissan/s15x/v1' },
+						]},
+					],
+				},
+				{
+					label: 'Opel ECUs',
+					items: [
+						{ label: 'BossECU C1XSE', items: [
+							{ label: 'V1', link: '/opel/c1xse/v1' },
+						]},
+					],
+				},
+				{
+					label: 'Bosch ECUs',
+					items: [
+						{ label: 'BossECU MSE6 (In development)', link: '#' },
+						{ label: 'BossECU MSE8 (In development)', link: '#' },
+					],
+				},
+				{
+					label: 'PSA ECUs',
+					items: [
+						{ label: 'BossECU PSA NFW (In development)', link: '#' },
+					],
+				},
+				{
+					label: 'Universal ECUs',
+					items: [
+						{ label: 'BossECU 48', items: [
+							{ label: 'V2.1b', link: '/universal/bossecu48/v2.1b' },
+						]},
+						{ label: 'Speeduino', items: [
+							{ label: 'V0.4.3d', link: '/universal/speeduino/v0.4.3d' },
+						]},
+						{ label: 'Speedy Delphi', items: [
+							{ label: 'V2', link: '/universal/speedy-delphi/v2' },
+						]},
+					],
+				},
+				{
+					label: 'Accessories & Tools',
+					items: [
+						{ label: 'Igniters', items: [
+							{ label: 'BossECU Igniter 4ch V1', link: '/igniters/4ch/v1' },
+						]},
+						{ label: 'Trigger Tools', items: [
+							{ label: 'DPFI disc', link: '#' },
+							{ label: 'Honda Cam Trigger (In development)', link: '#' },
+						]},
+					],
+				},
 			],
 		}),
 	],
