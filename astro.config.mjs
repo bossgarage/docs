6f74a3deb5b8deca01b49ab50d16ac4c031ddd69
Flávio Pereira <github@flaviopereira.dev>
2025-03-07 22:55:17 +0000

    added social links in astro cfg

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,10 @@ export default defineConfig({
 			},
 			social: {
 				github: 'https://github.com/bossgarage/docs',
+				discord: 'https://discord.gg/bossgarage',
+				youtube: 'https://youtube.com/@bossgarage',
+				facebook: 'https://facebook.com/bossgarage',
+				instagram: 'https://instagram.com/bossgarage',
 			},
 			sidebar: [
 				{
